9dce2678b7a7a249afaf65f51e7c8086f26bba77
Mudit Mudgal <mudgalmudit2003@gmail.com>
2026-08-10 21:15:54 +0530

    Datatypes

---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,10 @@
+//priorities readability
+
+//alert(3 + 3)  ->not work in nodejs
+
+//int
+//string
+//bool
+//null or means empty  0x0 -->type or standalone value
+//undefined
+//symbol  ==> uniqueness learn in (react)
