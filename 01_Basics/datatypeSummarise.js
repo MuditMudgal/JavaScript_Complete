577071e2b9e7008c98be5719a48743988107773a
Mudit Mudgal <mudgalmudit2003@gmail.com>
2026-08-10 23:15:03 +0530

    Summarise_datatypes

---
+++ 01_Basics/datatypeSummarise.js
@@ -0,0 +1,16 @@
+// types of datatypes are classified into two :- Primitive and Non Primitive
+
+//JS is dynamic language bc we cant define its datatype of variable name 
+
+//Non Primitive(Reference)
+// Arrays, Objects , Functions
+
+const id1 = Symbol('123')
+const id2 = Symbol('123');
+
+console.log(id1===id2)
+
+
+// let num = 643547654335434
+let num = 643547654335434n
+console.log(num)
