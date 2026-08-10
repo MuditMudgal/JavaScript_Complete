63b84b35782033a5598e970ac214565fc9637bbd
Mudit Mudgal <mudgalmudit2003@gmail.com>
2026-08-10 21:12:59 +0530

    Variables

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = "144453"
+let accontEmail = "mudgalmudit2003@gmail.com"
+var accountPassword = "12121212"
+accountCity = "Rohtak"
+
+//accountCity = "Rohtak";  not allowed
+
+let accountState;
+
+
+console.log(accountId)
+
+console.table([accountId, accontEmail, accountPassword, accountCity, accountState])
