82434aeb66e14dd113e1136de45cdf8715d6f217
Mudit Mudgal <mudgalmudit2003@gmail.com>
2026-08-10 22:15:10 +0530

    Updated_Conversions

--- 01_Basics/03_conversion.js
+++ 01_Basics/03_conversion.js
@@ -1,7 +1,16 @@
 // console.log(typeof("mudit"))
 
-let score = "33";
+// let score = "33";
 
-console.log(typeof score);
-let x = Number(score);
-console.log(typeof x);
+// console.log(typeof score);
+// let x = Number(score);
+// console.log(typeof x);
+
+// console.log(1 + 2 + "2");
+
+// console.log("1" + (2 + 2));
+
+
+
+let a = 100;
+console.log(a++) //postincreament  
