f243d0364971319d561abfda8b11cb720dd3473e
Mudit Mudgal <mudgalmudit2003@gmail.com>
2026-08-11 18:16:49 +0530

    Nums and Maths

---
+++ 01_Basics/06_numsAndMaths.js
@@ -0,0 +1,10 @@
+const v = 1000;
+
+const num = new Number(1000);
+console.log(num)
+
+const num1 = 100;
+console.log(num1.toFixed(2)) //.00
+
+const num2 = 12.398;
+console.log(num2.toPrecision(2)) //.00
