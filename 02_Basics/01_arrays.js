16ca024086552291779482cfde1d1ba4be16e0b0
Mudit Mudgal <mudgalmudit2003@gmail.com>
2026-08-13 12:50:46 +0530

    Arrays1_IntroAndMethods

---
+++ 02_Basics/01_arrays.js
@@ -0,0 +1,29 @@
+// const myarr = [0,1,2,3,4,5]
+// console.log(myarr)
+
+// const arr2 = new Array(1,2,3,4,5,6)
+// console.log(arr2)
+
+//Array Method
+
+//push
+// myarr.push(6)
+// console.log(myarr)
+// myarr.push(7);
+// console.log(myarr);
+// myarr.pop()
+
+//join
+// const arr = [0,1,2,3,4,5,6]
+// console.log(arr)
+// const newarr = arr.join();
+// console.log(newarr)
+
+//slice splice
+const myarr = [0,1,2,3,4,5]
+//in slice- original arr is not manipulated
+console.log("A ",myarr)  //print arr
+console.log(myarr.slice(1,3))  //print slice
+console.log("B ", myarr); //print arr
+console.log(myarr.splice(1,3)) //print splice
+console.log("C ", myarr);  //print arr
