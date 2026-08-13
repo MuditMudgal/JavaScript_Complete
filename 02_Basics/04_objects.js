ad94dd3776da6c304a457030825f4a588376a776
Mudit Mudgal <mudgalmudit2003@gmail.com>
2026-08-14 02:47:21 +0530

    json talks

--- 02_Basics/04_objects.js
+++ 02_Basics/04_objects.js
@@ -1,28 +1,27 @@
 // const tinderUser = new Object()  //singleton
-const tinderUser = {}
+const tinderUser = {};
 
-tinderUser.name = "mudit"
-tinderUser.email = "mudu@gmail.com"
-tinderUser.age = 23
-tinderUser.loggedin = true
+tinderUser.name = "mudit";
+tinderUser.email = "mudu@gmail.com";
+tinderUser.age = 23;
+tinderUser.loggedin = true;
 
 // console.log(tinderUser)
 
 const school = {
     name: "APEX School",
     class: {
-        tenth:{
-            section:{
+        tenth: {
+            section: {
                 classname: "apex-10th",
                 classsection: "A",
-                classTeacher: "Komal Garg"
-            }
-        }
-    }
-}
+                classTeacher: "Komal Garg",
+            },
+        },
+    },
+};
 
-// console.log(school.class.tenth.section.classTeacher)  
-
+// console.log(school.class.tenth.section.classTeacher)
 
 // const obj1 = { 1: "a", 2: "b" }
 // const obj2 = { 3: "c", 4: "d" }
@@ -30,34 +29,55 @@ const school = {
 // const obj4 = {...obj1,...obj2,...obj3}
 // console.log(obj4);
 
-const target = { a: 1 , b: 2 };
-const source = { b: 7, c: 9 }
-const final = Object.assign(source, target)
+const target = { a: 1, b: 2 };
+const source = { b: 7, c: 9 };
+const final = Object.assign(source, target);
 // console.log(final)
-
 
 // console.log(tinderUser)
 // console.log(Object.keys(tinderUser))
 // console.log(Object.values(tinderUser));
 // console.log(Object.entries(tinderUser))
 
-// console.log(tinderUser.hasOwnProperty('name')) //is it present or not 
-
+// console.log(tinderUser.hasOwnProperty('name')) //is it present or not
 
 //values comes from DB
 const users = [
     {
         id: 1,
-        username: "x"
+        username: "x",
     },
     {
         id: 2,
-        username: "y"
+        username: "y",
     },
     {
         id: 3,
-        username: "z"
-    }
+        username: "z",
+    },
 ];
 
-console.log(users[0].username)
+// console.log(users[0].username)
+
+const course = {
+    coursename: "js in hindi",
+    courseprice: "999",
+    courseinstructor: "Hitesh",
+};
+
+// console.log(course.courseinstructor)
+const { courseinstructor: instructor } = course;
+// console.log(courseinstructor)
+console.log(instructor);
+
+// {
+//     "name": "hitesh",
+//     "course": "js in hindi"
+//     "price": "999",
+// }
+
+[ 
+    {},
+    {},
+    {}
+]
