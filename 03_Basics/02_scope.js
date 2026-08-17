4745327201d3edbde369e7c62dcd3afab59a2c39
Mudit Mudgal <mudgalmudit2003@gmail.com>
2026-08-18 00:18:39 +0530

    scope adv

--- 03_Basics/02_scope.js
+++ 03_Basics/02_scope.js
@@ -11,4 +11,52 @@ if(true){
 // console.log(a); //a is not defined bc it followes block scope
 // console.log(b); //b is not defined bc it followes block scope
 
-console.log(x)
+// console.log(x)
+
+
+
+//main thing is like example bigger person and smaller person , smaller person take icecream from bigger person but bigger person does not take icecream from smaller person 
+//exaclty like this outer func() doesnot uses inner ❌ but inner take access to outer func()
+function one(){
+    const username = "Mudit"
+
+    function two(){
+        const website = "google"
+        console.log(username)
+    }
+
+    // console.log(website)
+
+    // two()
+}
+
+// one()
+
+//find the error
+if(true){
+    const username = "hitesh"
+    if(username === "hitesh"){
+        const website = " youtube"
+        console.log(username + website);
+    }
+    // console.log(username + website);
+}
+// console.log(username)
+
+
+//+++++++++++++ interesting +++++++++++++++++
+
+// there is two type of function writing
+console.log(addone(5));
+
+function addone(num){
+    return num+1
+}
+
+
+
+const addtwo = function(num){
+    return num+2
+}
+
+addtwo(5)
