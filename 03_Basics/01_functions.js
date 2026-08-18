0335748311d792e93098e033be11d9c4d77c1f85
Mudit Mudgal <mudgalmudit2003@gmail.com>
2026-08-19 00:14:43 +0530

    function

--- 03_Basics/01_functions.js
+++ 03_Basics/01_functions.js
@@ -1,3 +1,9 @@
+// function greet(name) {
+    // name = parameter
+// }
+
+// greet("Mudit");
+// "Mudit" = argument
 
 // function saymyname(){
 //     console.log("M");
@@ -18,15 +24,15 @@
 // add(3,"4") //34
 // add(3,"hello") //3hello
 
-
 function add(num1, num2) {
-    return (num1+num2);
+    return num1 + num2;
 }
 // const result = add(3,4)
 // console.log(result)
 // console.log(typeof result);
 
-function UserLoggedIn(username = "sam"){  //sam is taken as default value if there is no value from arguments 
+function UserLoggedIn(username = "sam") {
+    //sam is taken as default value if there is no value from arguments
     // if(username === undefined){
     //     console.log("Please enter a username");
     //     return;
@@ -40,29 +46,28 @@ function UserLoggedIn(username = "sam"){  //sam is taken as default value if the
 // console.log(user2);
 // console.log(UserLoggedIn())
 
-
 // ---------------------------FUNCTIONS 2-------------------------
 
-
-function calculateCartPrice(...num1){
-    return num1
+function calculateCartPrice(...num1) {
+    return num1;
 }
 
 // console.log(calculateCartPrice(200, 300, 400, 500, 600))
 const result = calculateCartPrice(200, 300, 400, 500, 600);
 // console.log(...result)
 
-
 //passing objects to function
 
 const user = {
-    username : "Mudit",
-    userid : 124551,
-    price : 5000 
-}
+    username: "Mudit",
+    userid: 124551,
+    price: 5000,
+};
 
-function handleObject(anyuser){
-    console.log(`${anyuser.username} is the username and price is ${anyuser.price}`)
+function handleObject(anyuser) {
+    console.log(
+        `${anyuser.username} is the username and price is ${anyuser.price}`,
+    );
 }
 
 // handleObject(user)
@@ -72,13 +77,12 @@ function handleObject(anyuser){
 //     price : 1000
 // })
 
+//passing arr to functions
+const myArr = [10, 20, 30, 40];
 
-//passing arr to functions 
-const myArr = [10, 20, 30, 40]
-
-function printArr(newarr){
-    console.log(`${newarr[0]} is first elemnt and last is ${newarr[3]}`)
+function printArr(newarr) {
+    console.log(`${newarr[0]} is first elemnt and last is ${newarr[3]}`);
 }
 
 // printArr(myArr)
-printArr([200,500,400,1000])
+printArr([200, 500, 400, 1000]);
