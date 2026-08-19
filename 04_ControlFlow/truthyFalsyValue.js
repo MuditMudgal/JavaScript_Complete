a5b5563fa16a08599d1d7b73dd3825da04e43793
Mudit Mudgal <mudgalmudit2003@gmail.com>
2026-08-19 13:08:30 +0530

    truthy falsy

---
+++ 04_ControlFlow/truthyFalsyValue.js
@@ -0,0 +1,50 @@
+const useremail = "m@mudit@gmail.com";
+
+// if (useremail) {
+//     console.log("Yes it have eamil");
+// } else {
+//     console.log("No it doesn't have Email");
+// }
+
+
+
+// >, <, <=, >=, ==, ===, !=
+//logical operators  && ||
+
+
+
+// const username = "";
+
+// if (username) {
+//     console.log("Yes it have username");
+// } else {
+//     console.log("No it doesn't have username");
+// }
+
+
+
+//falsy value
+// false, null, Nan, 0, -0, BigInt 0n, undefined, ""
+
+//truthy value
+// 'false', '0', " ", [], {}, function () {}
+
+
+
+
+
+//Nullish coalescing operator(??) /alternative fallback
+val1 = 5 ?? 10
+console.log(val1);
+
+val1 = null ?? 10
+console.log(val1);
+
+val1 = undefined ?? 15;
+console.log(val1);
+
+
+//terniary operator
+// condition ? true : false 
+const score = 1000
+score>=500 ? console.log("yes") : console.log("no");
