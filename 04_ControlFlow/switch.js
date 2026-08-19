44954891c67d5dcad9cd03f6390837ab5391d520
Mudit Mudgal <mudgalmudit2003@gmail.com>
2026-08-19 12:43:44 +0530

    switch

---
+++ 04_ControlFlow/switch.js
@@ -0,0 +1,41 @@
+// switch (key) {
+//     case value:
+
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = 15;
+
+switch (month) {
+    case 1:
+        console.log("Jan");
+        break;
+    case 2:
+        console.log("Feb");
+        break;
+    case 3:
+        console.log("March");
+        break;
+
+    case 4:
+        console.log("april");
+        break;
+    case 5:
+        console.log("May");
+        break;
+    case 6:
+        console.log("June");
+        break;
+    case 7:
+        console.log("July");
+        break;
+    case 8:
+        console.log("Aug");
+        break;
+
+    default: console.log("INVALID");
+        break;
+}
