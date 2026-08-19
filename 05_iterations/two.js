a8396fe88bcd686dec402ea8bfed4135d6afbacd
Mudit Mudgal <mudgalmudit2003@gmail.com>
2026-08-20 00:30:15 +0530

    while dowhile

---
+++ 05_iterations/two.js
@@ -0,0 +1,28 @@
+// let i = 0;
+// while(i<=10){
+//     console.log(i);
+//     i++;
+// }
+
+// let myarr = ["flash", "batman", "spiderman"]
+// let index = 0;
+// while(index < myarr.length){
+//     console.log(myarr[index]);
+//     index++;
+// }
+
+
+
+//printing score upto 10 with do while
+
+// let score = 1;
+// do{
+//     console.log(score);
+//     score++;
+// }while(score<=10);
+
+let score = 11;
+do {
+    console.log(`Score is ${score}`);
+    score++;
+} while (score <= 10);
