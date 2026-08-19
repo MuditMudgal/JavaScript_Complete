e22e141a8012708d491127cb2568cc8ab2ea2f7f
Mudit Mudgal <mudgalmudit2003@gmail.com>
2026-08-19 12:33:46 +0530

    if else elseif

---
+++ 04_ControlFlow/one.js
@@ -0,0 +1,29 @@
+
+if(2=="2"){
+    console.log("true");
+}
+
+// if(){
+
+// }
+// else{
+
+// }
+
+
+// const score = 200
+// if(score > 100){
+//     const power = 'fly'
+//     console.log(`power is : ${power}`)
+// }
+// else{
+//     console.log(`power2 is : ${power}`);   
+// }
+
+
+// if
+// else if
+// else if
+// else
+
+// && ||
