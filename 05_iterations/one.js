6a7ab7363e57d38d1ca6fccd31f7c55231db9870
Mudit Mudgal <mudgalmudit2003@gmail.com>
2026-08-20 00:19:11 +0530

    loops or iterations

---
+++ 05_iterations/one.js
@@ -0,0 +1,21 @@
+//for loop
+
+// for (let index = 0; index < 10; index++) {
+//     const element = index;
+//     console.log(element);
+// }
+
+for(let i=2; i<=2 ; i++){
+    for(let j=1; j<=10 ; j++){
+        // console.log(i + ' * ' + j + ' = ' + i*j);
+    }
+}
+
+
+//printing array
+let myarr = [1,2,3,4,50,6]
+for(let i=0;i<myarr.length;i++){
+    console.log(myarr[i]);
+}
+
+//break and continue keyword
