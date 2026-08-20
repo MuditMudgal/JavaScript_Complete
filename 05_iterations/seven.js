c15d40f01d1e3e6408efc21dd3d4cabd92238ae4
Mudit Mudgal <mudgalmudit2003@gmail.com>
2026-08-20 23:30:26 +0530

    map and chaining

---
+++ 05_iterations/seven.js
@@ -0,0 +1,14 @@
+const mynumber = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+//map returns value but for each doesnot return 
+const ans = mynumber.map( (num)=>{
+    return (num+10)
+} )
+// console.log(ans)
+
+
+//chaining of methods
+const multAdd = mynumber.map( (num) => num*10 )
+                        .map( (num) => num+1 )
+                        .filter( (num) => num>40)
+console.log(multAdd)
