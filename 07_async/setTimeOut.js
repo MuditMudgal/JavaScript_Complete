ca121ae374424d10d4d79ed9279dfffd031b73ac
Mudit Mudgal <mudgalmudit2003@gmail.com>
2026-08-22 11:54:03 +0530

    async setTimeOut

---
+++ 07_async/setTimeOut.js
@@ -0,0 +1,17 @@
+// console.log('start 1')
+
+// setTimeout(() => {
+//     console.log('I am from setTimeOut');
+// }, 5000)
+
+// console.log('Start 2');
+// console.log('End')
+
+console.log("start 1");
+
+setTimeout(() => {
+    console.log("I am from setTimeOut");
+}, 0);
+
+console.log("Start 2");
+console.log("End");
